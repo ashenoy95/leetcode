164ea513800317aa9fcb9abe6e596839ee5e0413
ashenoy95 <shenoyaniket95@gmail.com>
2017-10-09 21:28:01 -0400

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -5,15 +5,11 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        #O(nlogn)
-        nums_sorted = sorted(nums)
-        i = 0
-        j = len(nums) - 1
-        while i<j:
-            if nums_sorted[i]+nums_sorted[j]>target:
-                j-=1
-            if nums_sorted[i]+nums_sorted[j]<target:
-                i+=1
+        hash = {}
+        for i in range(len(nums)):
+            if nums[i] not in hash:
+                hash[target-nums[i]] = i
             else:
-                return [nums.index(nums_sorted[i]), nums.index(nums_sorted[j])]
-       
+                return [hash[nums[i]], i]
+                
+                
