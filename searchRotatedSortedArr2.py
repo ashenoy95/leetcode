00cd8da80437957d5b19b611ec0c5b43c9e161f6
Aniket Shenoy <shenoyaniket95@gmail.com>
2018-01-29 19:42:26 -0500

    Add files via upload

---
+++ searchRotatedSortedArr2.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def binarySearch(self, nums, left, right, target):
+        if left>right:
+            return False
+        mid = (left+right)//2
+        if nums[mid]==target:
+            return True
+        if nums[left]<nums[mid]:
+            if target>=nums[left] and target<nums[mid]:
+                return self.binarySearch(nums, left, mid, target)
+            else:
+                return self.binarySearch(nums, mid+1, right, target)
+        elif nums[mid]<nums[left]:
+            if target>nums[mid] and target<=nums[right]:
+                return self.binarySearch(nums, mid+1, right, target)
+            else:
+                return self.binarySearch(nums, left, mid, target)
+        elif nums[mid]==nums[left]:
+            if nums[mid]!=nums[right]:
+                return self.binarySearch(nums, mid+1, right, target)
+            else:
+                res = self.binarySearch(nums, left, mid-1, target)
+                if not res:
+                    return self.binarySearch(nums, mid+1, right, target)
+                else:
+                    return res
+    
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return False
+        return self.binarySearch(nums, 0, len(nums)-1, target)
+    
+    
+    
