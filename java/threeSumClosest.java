bb164289e9119920095783053390246cf6bc259e
Aniket Shenoy <aniketshenoy@149-160-173-97.dhcp-bl.indiana.edu>
2018-03-23 19:35:33 -0400

    changed to java

--- java/threeSumClosest.java
+++ java/threeSumClosest.java
@@ -1,28 +1,26 @@
-class Solution(object):
-    def threeSumClosest(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        nums.sort()
-        closest = sum(nums[:3])
-        for i in range(len(nums)-2):
-            j = i+1
-            k = len(nums)-1
-            while j<k:
-                print(nums[i], nums[j], nums[k])
-                sum3 = nums[i]+nums[j]+nums[k]
-                print(sum3, closest)
-                print()
-                if sum3==target:
-                    return target
-                if abs(target-sum3)<abs(target-closest):
-                    closest = sum3
-                if sum3<target:
-                    j += 1
-                else:
-                    k -= 1
-        return closest
-            
-            
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int j, k, sum;
+        Arrays.sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+        
+        for(int i=0; i<nums.length-2; i++) {
+            j = i+1;
+            k = nums.length-1;
+            while(j<k) {
+                sum = nums[i]+nums[j]+nums[k];
+                if(sum==target)
+                    return target;
+                if(sum<target) {
+                    j++;
+                } else {
+                    k--;
+                }
+                if(Math.abs(target-sum)<Math.abs(target-closest))
+                    closest = sum;
+            }
+        }
+        
+        return closest;
+    }
+}
