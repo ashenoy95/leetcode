78307f1675e7ee66ca58de93513a676a9f50734e
Aniket Shenoy <aniketshenoy@149-160-173-97.dhcp-bl.indiana.edu>
2018-03-23 20:56:17 -0400

    fixed 3Sum.py and added java

--- python/threeSum.py
+++ python/threeSum.py
@@ -6,19 +6,32 @@ class Solution(object):
         """
         soln = []
         nums.sort()
-        for i in range(len(nums)-2):
+        
+        for i in range(len(nums)-2):  
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+                
             j = i+1
             k = len(nums)-1
+            
             while j<k:
                 sum3 = nums[i]+nums[j]+nums[k]
+                
                 if sum3==0:
                     soln.append([nums[i], nums[j], nums[k]])
-                    while j<k and nums[j]==nums[j+1]:
+                    j += 1
+                    k -= 1
+                    while j<k and nums[j]==nums[j-1]:
                         j += 1
-                    while k>j and nums[k]==nums[k-1]:
+                    while j<k and nums[k]==nums[k+1]:
                         k -= 1
-                if sum3<0:
+                elif sum3<0:
                     j += 1
                 else:
                     k -= 1
-        return list(set(map(tuple, soln)))
+                    
+        return soln
+            
+    
+    
+    
