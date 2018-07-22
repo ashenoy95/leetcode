e70267865382175a5b973c71e9bd6ca32d185f84
Aniket Shenoy <aniketshenoy@Anikets-MacBook-Pro.local>
2018-07-21 20:28:33 -0700

    reduced runtime

--- python/perms.py
+++ python/perms.py
@@ -1,20 +1,18 @@
 class Solution(object):
-    def swap(self, nums, i, left):
-        nums[i], nums[left] = nums[left], nums[i]
-
     def perm(self, nums, left, final):
         if left==len(nums):
-            final.append([num for num in nums])
-            return 
+            final.append(nums[:])
+            
         for i in range(left, len(nums)):
-            self.swap(nums, i, left)
+            nums[i], nums[left] = nums[left], nums[i]
             self.perm(nums, left+1, final)
-            self.swap(nums, i, left)
-        return final
+            nums[i], nums[left] = nums[left], nums[i]
 
     def permute(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        return self.perm(nums, 0, [])
+        perms = []
+        self.perm(nums, 0, perms)
+        return perms
