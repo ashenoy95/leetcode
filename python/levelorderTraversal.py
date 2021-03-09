af3b39bc8e05bab5ce04c9f4f2a7e9dff0b8a25f
Aniket Shenoy <shenoyaniket95@gmail.com>
2021-03-08 23:38:32 -0800

    replaced with more intuitive solution

--- python/levelorderTraversal.py
+++ python/levelorderTraversal.py
@@ -1,31 +1,39 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
 
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root:
-            return []
-        else:
-            q = [root]
-        level_order = []
+            return root
         
-        while q:
-            level_order.append([node.val for node in q])
+        queue = deque([root])
+        level_order = []
 
-            curr_level = [node for node in q]
-            q = []
-            for succ in curr_level:
-                if succ.left:
-                    q.append(succ.left) 
-                if succ.right:
-                    q.append(succ.right)
+        while queue:
+            # l keeps tracks of how many nodes in queue at a certain level because only immediate children will be in queue
+            # level_indicator increments every time a node is processed 
+            # when l == level_indicator, it means all nodes in a level are processed
+            l = len(queue)
+            level_indicator = 0
+            level = []
+
+            while level_indicator < l:
+                node = queue.popleft()
+                level.append(node.val)
+                level_indicator += 1
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            level_order.append(level)
             
+        
         return level_order
+        
