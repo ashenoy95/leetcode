f7658ca360ccd944824653fe6a9e28990ae76058
Aniket Shenoy <shenoyaniket95@gmail.com>
2021-03-10 23:42:24 -0800

    added dfs solution

--- python/rightSideView.py
+++ python/rightSideView.py
@@ -4,7 +4,10 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# BFS
 from collections import deque
+
 
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
@@ -31,3 +34,25 @@ class Solution:
             
         return right_side_view
     
+    # DSF    
+    def _dfs(self, root, level, level_max, right_side_view):
+        if not root:
+            return root
+        
+        if level > level_max:
+            right_side_view.append(root.val)
+            
+        self._dfs(root.right, level+1, len(right_side_view), right_side_view)
+        self._dfs(root.left, level+1, len(right_side_view), right_side_view)
+        
+    
+    def rightSideView_dfs(self, root: TreeNode) -> List[int]:
+        right_side_view = []
+        node = root
+        while node:
+            right_side_view.append(node.val)
+            node = node.right
+            
+        self._dfs(root, level=1, level_max=len(right_side_view), right_side_view=right_side_view)
+        
+        return right_side_view
