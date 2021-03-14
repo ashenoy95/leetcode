07b84cb73645e48d197daa851fe9da7d2d750747
Aniket Shenoy <shenoyaniket95@gmail.com>
2021-03-13 16:15:00 -0800

    rm comments

--- python/lowestCommonAncestor.py
+++ python/lowestCommonAncestor.py
@@ -6,36 +6,18 @@
 #         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        if root:
-            print('\ncurr node:', root.val if root else root)
-        if not root or p.val==root.val or q.val==root.val:
-            print('node is null or node = p or q so returning')
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if (not root) or (root.val == p.val) or (root.val == q.val):
             return root
-        print('curr node:', root.val if root else root)
-        print('calling left')
+        
         left = self.lowestCommonAncestor(root.left, p, q)
-        print('curr node:', root.val if root else root)
-        print('left:', left.val if left else left)
-        print('calling right')
         right = self.lowestCommonAncestor(root.right, p, q)
         
         if left and right:
-            print('left: {}, right: {}'.format(left.val, right.val))
-            print('both left and right exist so returning\n', root.val)
             return root
         
         if left:
-            print('left exists so returning i.e. second node is not in right subtree\n', left.val)
             return left
         if right:
-            print('right exists so returning\n', right.val)
             return right
-        print('left and right are both null\n')
-        return 
+    
