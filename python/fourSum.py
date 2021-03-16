5a7eb0e196a8171431f9950b3b242a63892cf13a
Aniket Shenoy <shenoyaniket95@gmail.com>
2021-03-16 12:26:30 -0700

    generic solution

--- python/fourSum.py
+++ python/fourSum.py
@@ -1,37 +1,55 @@
 class Solution:
-    def fourSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        sums = {}
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                s = nums[i]+nums[j]
-                if s not in sums:
-                    sums[s] = [[i, j]]
-                else:
-                    sums[s].append([i, j])
+    """
+    3Sum wraps 2Sum in an outer loop, similarly 4Sum can be built up as 3Sum wrapped in another
+    loop. This can be generalized to kSum using k-2 loops over 2Sum. 
+    Time: O(n^(k-1))
+    Space: O(n) for recursion stack
+    Reference: https://leetcode.com/problems/4sum/solution/
+    """
+    def twoSum(self, nums, target):
+        result = []
+        i = 0
+        j = len(nums) - 1
+        
+        while i < j:
+            s = nums[i] + nums[j] 
+            
+            if (s > target) or ((j < len(nums) - 1) and (nums[j] == nums[j+1])):
+                j -= 1
+            elif (s < target) or ((i > 0) and (nums[i] == nums[i-1])):
+                i += 1
+            else:
+                result.append([nums[i], nums[j]])
+                i += 1
+                j -= 1
+        
+        return result
+    
+    
+    def kSum(self, nums, target, k):
+        print()
+        print('nums:', nums)
+        print('k:', k)
+        print('target:', target)
+        result = []
+        if (not nums) or (sum(nums[:k]) > target) or (sum(nums[-k:]) < target):
+            return []
+        
+        if k == 2:
+            return self.twoSum(nums, target)
+        
+        for i in range(len(nums)):
+            if (i > 0) and (nums[i] == nums[i-1]):
+                continue
                 
-        soln = []
-        # print('target:', target)
-        # print(nums)
-        # print(sums)
-        # print()
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                s = nums[i]+nums[j]
-                #print('i: {}, j:{}, s:{}, target-s:{}'.format(i, j, s, target-s))
-                if target-s in sums:
-                    pairs = sums[target-s]
-                    for pair in pairs:
-                        if pair[0]!=i and pair[0]!=j and pair[1]!=i and pair[1]!=j:
-                            four_sum = sorted([nums[i], nums[j], nums[pair[0]], nums[pair[1]]])
-                            # print('fourSum:', four_sum)
-                            # print()
-                            if four_sum not in soln:
-                                soln.append(four_sum)
-            #print()
-        #print(soln)
-        return soln
+            k_minus_sums = self.kSum(nums[i+1:], target-nums[i], k-1)
+            for s in k_minus_sums:
+                result.append([nums[i]] + s)
+                print(result)
+    
+        return result
+        
+        
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        return self.kSum(sorted(nums), target, k=4)
+    
