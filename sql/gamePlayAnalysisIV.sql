5c7d46183529fe6f05609e9ba86c38c52ed3aacd
Aniket Shenoy <shenoyaniket95@gmail.com>
2025-08-03 15:16:25 -0700

    adding optimized method

--- sql/gamePlayAnalysisIV.sql
+++ sql/gamePlayAnalysisIV.sql
@@ -19,3 +19,30 @@ from
 where
     login_order = 1
 ;
+
+-- method 2: join (optimized)
+with first_login as 
+(
+select
+    player_id, 
+    min(event_date) as event_date
+from
+    activity
+group by 1
+)
+
+select  
+    round(1.0*sum(
+        case
+            when a.player_id is not null then 1 
+            else 0
+        end
+    )/count(fl.player_id), 2) as fraction
+from 
+    first_login fl
+left join
+    activity a
+on
+    fl.player_id = a.player_id
+    and a.event_date = fl.event_date+1
+;
