ff3f3d2504cf2f21e2d147a8bef30ebca3798caa
Aniket Shenoy <shenoyaniket95@gmail.com>
2025-08-03 14:15:14 -0700

    Create monthlyTransactionsI.sql

---
+++ sql/monthlyTransactionsI.sql
@@ -0,0 +1,22 @@
+-- Write your PostgreSQL query statement below
+select
+    to_char(date_trunc('month', trans_date), 'YYYY-MM') as month,
+    country,
+    count(distinct id) as trans_count,
+    sum(
+        case 
+            when state = 'approved' then 1
+            else 0
+        end
+    ) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(
+        case 
+            when state = 'approved' then amount
+            else 0
+        end
+    ) as approved_total_amount
+from
+    transactions
+group by 1, 2
+;
